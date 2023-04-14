55e27b85866d38b2e3b7b0cb0dd3062ef04f70f7
yubinkarki <iamyubinkarki@gmail.com>
2023-04-14 08:23:41 +0545

    Create new register view

---
+++ lib/views/register_view.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import "package:firebase_auth/firebase_auth.dart";
+
+import 'package:okaychata/firebase_options.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Container(
+                color: Colors.teal[50],
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _email,
+                        keyboardType: TextInputType.emailAddress,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: const InputDecoration(
+                          hintText: "Enter your email",
+                        ),
+                      ),
+                      TextField(
+                        controller: _password,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: const InputDecoration(
+                          hintText: "Enter your password",
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          final email = _email.text;
+                          final password = _password.text;
+
+                          try {
+                            final response = await FirebaseAuth.instance
+                                .createUserWithEmailAndPassword(email: email, password: password);
+
+                            print("Register response: $response");
+                          } on FirebaseAuthException catch (e) {
+                            if (e.code == "weak-password") {
+                              print("Password is too weak.");
+                            } else if (e.code == "email-already-in-use") {
+                              print("This email is already in use.");
+                            } else {
+                              print("Something else happened, $e");
+                            }
+                          }
+                        },
+                        child: const Text("Register"),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+
+            default:
+              return const Text("Loading");
+          }
+        },
+      ),
+    );
+  }
+}
