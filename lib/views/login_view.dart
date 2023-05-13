1c3b82ab33f5cc3fe317407fbcc82e56c20f6a1f
yubinkarki <iamyubinkarki@gmail.com>
2023-05-13 20:30:08 +0545

    Add scaffold as main return widget

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -30,51 +30,68 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal[50],
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              controller: _email,
-              keyboardType: TextInputType.emailAddress,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: "Enter your email",
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Container(
+        color: Colors.teal[50],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: _email,
+                keyboardType: TextInputType.emailAddress,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: "Enter your email",
+                ),
               ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: "Enter your password",
+              TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: "Enter your password",
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: TextButton(
+                  onPressed: () async {
+                    final email = _email.text;
+                    final password = _password.text;
 
-                try {
-                  final response =
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+                    try {
+                      final response =
+                          await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
 
-                  print("Login response: $response");
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == "user-not-found") {
-                    print("User not found.");
-                  } else if (e.code == "wrong-password") {
-                    print("Incorrect password.");
-                  }
-                }
-              },
-              child: const Text("Login"),
-            ),
-          ],
+                      print("Login response: $response");
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == "user-not-found") {
+                        print("User not found.");
+                      } else if (e.code == "wrong-password") {
+                        print("Incorrect password.");
+                      }
+                    }
+                  },
+                  child: const Text("Login"),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/register/',
+                    (route) => false,
+                  );
+                },
+                child: const Text("Go to Register"),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -33,53 +33,70 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal[50],
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              controller: _email,
-              keyboardType: TextInputType.emailAddress,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: "Enter your email",
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Container(
+        color: Colors.teal[50],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: _email,
+                keyboardType: TextInputType.emailAddress,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: "Enter your email",
+                ),
               ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: "Enter your password",
+              TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: "Enter your password",
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: TextButton(
+                  onPressed: () async {
+                    final email = _email.text;
+                    final password = _password.text;
 
-                try {
-                  final response =
-                      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+                    try {
+                      final response =
+                          await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
 
-                  print("Register response: $response");
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == "weak-password") {
-                    print("Password is too weak.");
-                  } else if (e.code == "email-already-in-use") {
-                    print("This email is already in use.");
-                  } else {
-                    print("Something else happened, $e");
-                  }
-                }
-              },
-              child: const Text("Register"),
-            ),
-          ],
+                      print("Register response: $response");
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == "weak-password") {
+                        print("Password is too weak.");
+                      } else if (e.code == "email-already-in-use") {
+                        print("This email is already in use.");
+                      } else {
+                        print("Something else happened, $e");
+                      }
+                    }
+                  },
+                  child: const Text("Register"),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/login/',
+                    (route) => false,
+                  );
+                },
+                child: const Text("Go to Login"),
+              )
+            ],
+          ),
         ),
       ),
     );
