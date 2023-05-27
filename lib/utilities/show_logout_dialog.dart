649ac048f21ab5b8ff8098c8de11db565d9ef1e3
yubinkarki <iamyubinkarki@gmail.com>
2023-05-27 08:39:04 +0545

    Utility functions

---
+++ lib/utilities/show_logout_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Log Out"),
+        content: const Text("Are you sure you want to log out?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text("Log Out"),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
+}
