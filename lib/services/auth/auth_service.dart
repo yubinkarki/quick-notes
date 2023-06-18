4ed95df3600e9bfef0b9483d9b4da78634d34899
yubinkarki <iamyubinkarki@gmail.com>
2023-06-18 20:27:47 +0545

    New service extending auth provider

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,37 @@
+import 'package:okaychata/services/auth/auth_user.dart';
+import 'package:okaychata/services/auth/auth_provider.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+
+  const AuthService(this.provider);
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+
+  @override
+  Future<AuthUser> signUp({
+    required String email,
+    required String password,
+  }) =>
+      provider.signUp(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> logOut() => provider.logOut();
+}
