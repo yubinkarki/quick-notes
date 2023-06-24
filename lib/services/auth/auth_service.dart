d99aa421cc50798ad25a979da0d61290c9cfd8b3
yubinkarki <iamyubinkarki@gmail.com>
2023-06-24 17:40:45 +0545

    Initialize firebase

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,10 +1,16 @@
 import 'package:okaychata/services/auth/auth_user.dart';
 import 'package:okaychata/services/auth/auth_provider.dart';
+import 'package:okaychata/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider provider;
 
   const AuthService(this.provider);
+
+  factory AuthService.factoryFirebase() => AuthService(FirebaseAuthProvider());
+
+  @override
+  Future<void> initialize() => provider.initialize();
 
   @override
   AuthUser? get currentUser => provider.currentUser;
