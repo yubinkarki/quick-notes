b12c6eb3197d756bf54c99eb4da1004541fd9fb9
yubinkarki <iamyubinkarki@gmail.com>
2023-07-07 20:35:27 +0545

    Notes table column names

---
+++ lib/constants/notes.dart
@@ -0,0 +1,8 @@
+const dbName = "notes.db";
+const noteTable = "note";
+const userTable = "user";
+const idColumn = "id";
+const userIdColumn = "user_id"; // This is FK in 'notes' db.
+const emailColumn = "email";
+const textColumn = "text";
+const isSyncedWithCloudColumn = "is_synced_with_cloud";
