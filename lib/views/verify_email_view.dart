8196fcbf2d81968ba5a82f68ffb65ae443394fd1
yubinkarki <iamyubinkarki@gmail.com>
2023-07-10 17:18:40 +0545

    Add missing trailing comma

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -14,60 +14,61 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Verify your email"),
-        ),
-        body: Row(
-          children: [
-            Flexible(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(left: 50, right: 50),
-                    margin: const EdgeInsets.only(bottom: 20),
-                    child: const Text(
-                      "We've sent you an email verification. Please open it to verify your account.",
-                      textAlign: TextAlign.center,
-                    ),
+      appBar: AppBar(
+        title: const Text("Verify your email"),
+      ),
+      body: Row(
+        children: [
+          Flexible(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  margin: const EdgeInsets.only(bottom: 20),
+                  child: const Text(
+                    "We've sent you an email verification. Please open it to verify your account.",
+                    textAlign: TextAlign.center,
                   ),
-                  Container(
-                    padding: const EdgeInsets.only(left: 50, right: 50),
-                    child: const Text(
-                      "If you haven't received a verification email yet, press the button below.",
-                      textAlign: TextAlign.center,
-                    ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: const Text(
+                    "If you haven't received a verification email yet, press the button below.",
+                    textAlign: TextAlign.center,
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 30),
-                    child: TextButton(
-                      onPressed: () async {
-                        AuthService.factoryFirebase().sendEmailVerification();
-                      },
-                      child: const Text("Send email verification"),
-                    ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: TextButton(
+                    onPressed: () async {
+                      AuthService.factoryFirebase().sendEmailVerification();
+                    },
+                    child: const Text("Send email verification"),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 10),
-                    child: TextButton(
-                      onPressed: () async {
-                        AuthService.factoryFirebase().logOut();
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: TextButton(
+                    onPressed: () async {
+                      AuthService.factoryFirebase().logOut();
 
-                        if (!mounted) return;
+                      if (!mounted) return;
 
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                          loginRoute,
-                          (route) => false,
-                        );
-                      },
-                      child: const Text("Go to Login screen"),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ));
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        loginRoute,
+                        (route) => false,
+                      );
+                    },
+                    child: const Text("Go to Login screen"),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
