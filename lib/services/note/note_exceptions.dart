8f2218738d4c8cd4fe1ce00aa78951c19de5eb5e
yubinkarki <iamyubinkarki@gmail.com>
2023-07-10 17:08:57 +0545

    Add new exceptions for note service

--- lib/services/note/note_exceptions.dart
+++ lib/services/note/note_exceptions.dart
@@ -1,3 +1,11 @@
 class DatabaseAlreadyOpenException implements Exception {}
 
 class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseNotOpenException implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlreadyExistsException implements Exception {}
+
+class UserDoesNotExistException implements Exception {}
