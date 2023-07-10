f9feb717d3c41169d58099007e694da6574018ca
yubinkarki <iamyubinkarki@gmail.com>
2023-07-10 17:08:42 +0545

    New sql query constants

--- lib/constants/notes.dart
+++ lib/constants/notes.dart
@@ -6,3 +6,18 @@ const userIdColumn = "user_id"; // This is FK in 'notes' db.
 const emailColumn = "email";
 const textColumn = "text";
 const isSyncedWithCloudColumn = "is_synced_with_cloud";
+
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
+        "id" INTEGER NOT NULL,
+        "email" TEXT NOT NULL UNIQUE,
+        PRIMARY KEY("id" AUTOINCREMENT));
+        ''';
+
+const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
+        "id" INTEGER NOT NULL,
+        "user_id" INTEGER NOT NULL,
+        "text" TEXT,
+        "is_synced_with_cloud" INTEGER NOT NULL DEFAULT 0,
+        FOREIGN KEY("user_id") REFERENCES "user"("id"),
+        PRIMARY KEY("id" AUTOINCREMENT));
+        ''';
