a89c4eab412144fb72e34349fd9347589aae8270
yubinkarki <iamyubinkarki@gmail.com>
2023-07-11 16:00:49 +0545

    Separate file for home screen

---
+++ lib/views/home_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'login_view.dart';
+import 'notes_view.dart';
+import 'verify_email_view.dart';
+import 'package:okaychata/services/auth/auth_service.dart' show AuthService;
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: AuthService.factoryFirebase().initialize(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.factoryFirebase().currentUser;
+
+            if (user != null) {
+              if (!user.isEmailVerified) {
+                return const VerifyEmailView();
+              } else {
+                return const NotesView();
+              }
+            } else {
+              return const LoginView();
+            }
+
+          default:
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+        }
+      },
+    );
+  }
+}
