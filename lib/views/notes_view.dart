99b6ccc38ded67e7c48e6556b2970551812df85a
yubinkarki <iamyubinkarki@gmail.com>
2023-07-14 20:00:41 +0545

    Return  stream builder in a future builder

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:okaychata/constants/routes.dart';
 import 'package:okaychata/enums/menu_action.dart' show MenuAction;
 import 'package:okaychata/services/auth/auth_service.dart' show AuthService;
+import 'package:okaychata/services/note/note_service.dart' show NoteService;
 import 'package:okaychata/utilities/show_logout_dialog.dart' show showLogoutDialog;
 
 class NotesView extends StatefulWidget {
@@ -13,6 +14,25 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
+  late final NoteService _noteService;
+
+  String get userEmail => AuthService.factoryFirebase().currentUser!.email!;
+
+  @override
+  void initState() {
+    _noteService = NoteService();
+    _noteService.open();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _noteService.close();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,15 +76,45 @@ class _NotesViewState extends State<NotesView> {
           ),
         ],
       ),
-      body: Container(
-        color: Colors.green[200],
-        alignment: Alignment.center,
-        child: const Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 40,
-          ),
-        ),
+      body: FutureBuilder(
+        future: _noteService.getOrCreateUser(email: userEmail),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return StreamBuilder(
+                stream: _noteService.allNotes,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Container(
+                        color: Colors.green[200],
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Waiting for all notes...",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                      );
+
+                    default:
+                      return const Scaffold(
+                        body: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                  }
+                },
+              );
+
+            default:
+              return const Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+          }
+        },
       ),
     );
   }
