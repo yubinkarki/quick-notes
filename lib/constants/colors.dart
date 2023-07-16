396295af53951b85ed6dc2d10bd7d206ade4a37d
yubinkarki <iamyubinkarki@gmail.com>
2023-07-16 10:31:54 +0545

    Create new color constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color black = Color.fromRGBO(0, 0, 0, 1.0);
+  static const Color dark = Color.fromRGBO(29, 29, 29, 1.0);
+  static const Color teal = Color.fromRGBO(16, 155, 147, 1.0);
+  static const Color gray = Color.fromRGBO(138, 138, 138, 1.0);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color darkTeal = Color.fromRGBO(11, 108, 103, 1.0);
+  static const Color lightTeal = Color.fromRGBO(236, 255, 253, 1.0);
+  static const Color slightlyDark = Color.fromRGBO(38, 38, 38, 1.0);
+  static const Color slightlyWhite = Color.fromRGBO(242, 242, 242, 1.0);
+}
