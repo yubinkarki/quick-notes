eae6c92be477ad5bdfad3cffae7ff25099562207
yubinkarki <iamyubinkarki@gmail.com>
2023-07-16 10:32:30 +0545

    New text themes with font specifications

---
+++ lib/themes/dark_text_theme.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:okaychata/constants/colors.dart';
+
+TextTheme darkTextTheme(TextTheme base) {
+  return base.copyWith(
+    labelSmall: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+      color: CustomColors.slightlyWhite,
+      letterSpacing: 0.5,
+    ),
+    labelMedium: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      color: CustomColors.slightlyWhite,
+      letterSpacing: 0.5,
+    ),
+    labelLarge: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 22,
+      color: CustomColors.slightlyWhite,
+      letterSpacing: 0.5,
+    ),
+
+    // Used by input field text.
+    titleSmall: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+      color: CustomColors.slightlyWhite,
+      letterSpacing: 0.5,
+    ),
+    titleMedium: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      color: CustomColors.slightlyWhite,
+      letterSpacing: 0.5,
+    ),
+    titleLarge: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 22,
+      color: CustomColors.slightlyWhite,
+      letterSpacing: 0.5,
+    ),
+  );
+}
---
+++ lib/themes/light_text_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
+
+import 'package:okaychata/constants/colors.dart';
+
+TextTheme lightTextTheme(TextTheme base) {
+  return base.copyWith(
+    labelSmall: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+      color: CustomColors.darkTeal,
+      letterSpacing: 0.5,
+    ),
+    labelMedium: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      color: CustomColors.darkTeal,
+      letterSpacing: 0.5,
+    ),
+    labelLarge: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 22,
+      color: CustomColors.darkTeal,
+      letterSpacing: 0.5,
+    ),
+
+    // Used by input field text.
+    titleSmall: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+      color: CustomColors.darkTeal,
+      letterSpacing: 0.5,
+    ),
+    titleMedium: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      color: CustomColors.darkTeal,
+      letterSpacing: 0.5,
+    ),
+    // Appbar title.
+    titleLarge: GoogleFonts.quicksand(
+      fontWeight: FontWeight.w600,
+      fontSize: 22,
+      color: CustomColors.white,
+      letterSpacing: 0.5,
+    ),
+  );
+}
