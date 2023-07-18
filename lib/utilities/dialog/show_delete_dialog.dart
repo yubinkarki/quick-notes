ad1f1a69f2c526d3f6f7723ac51d02537b0f0b5f
yubinkarki <iamyubinkarki@gmail.com>
2023-07-18 18:42:38 +0545

    Refactor dialogs to separate folder

---
+++ lib/utilities/dialog/show_delete_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+Future<bool> showLDeleteDialog(BuildContext context) {
+  final TextTheme textTheme = Theme.of(context).textTheme;
+
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("Logout", style: textTheme.labelLarge),
+        content: Text(
+          "Are you sure you want to delete this item?",
+          style: textTheme.labelMedium,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: Text("No", style: textTheme.labelMedium),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: Text("Yes", style: textTheme.labelMedium),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
+}
