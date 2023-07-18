42ba533965e4395c65ac1b97c6616e5e62e14e97
yubinkarki <iamyubinkarki@gmail.com>
2023-07-18 18:44:15 +0545

    New widget for listing notes

---
+++ lib/views/note/note_list_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:okaychata/services/note/note_service.dart';
+import 'package:okaychata/utilities/dialog/show_delete_dialog.dart';
+
+typedef DeleteNoteCallback = void Function(DatabaseNote note);
+
+class NoteListView extends StatelessWidget {
+  final List<DatabaseNote> notes;
+  final DeleteNoteCallback onDeleteNote;
+
+  const NoteListView({
+    Key? key,
+    required this.notes,
+    required this.onDeleteNote,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+
+        return ListTile(
+          title: Text(
+            note.text,
+            maxLines: 1,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: IconButton(
+            onPressed: () async {
+              final shouldDelete = await showLDeleteDialog(context);
+
+              if (shouldDelete) {
+                onDeleteNote(note);
+              }
+            },
+            icon: const Icon(Icons.delete),
+          ),
+        );
+      },
+    );
+  }
+}
