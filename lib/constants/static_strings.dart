139ab4fa283b9c1b2377c56ff116976f70f4ce8c
yubinkarki <iamyubinkarki@gmail.com>
2023-08-02 06:49:12 +0545

    Create constants for static string values

---
+++ lib/constants/static_strings.dart
@@ -0,0 +1,9 @@
+class AppStrings {
+  static const String login = "Login";
+  static const String enterEmail = "Enter your email";
+  static const String enterPassword = "Enter your password";
+  static const String noUser = "User not found";
+  static const String incorrectPassword = "Incorrect password";
+  static const String authError = "Authentication error";
+  static const String goToRegister = "Go to Register";
+}
