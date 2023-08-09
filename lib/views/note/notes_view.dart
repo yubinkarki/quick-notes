45ed1bf6b3266e7440e09e7df6e10722c895b98b
yubinkarki <iamyubinkarki@gmail.com>
2023-08-09 18:38:36 +0545

    Add fake delay

--- lib/views/note/notes_view.dart
+++ lib/views/note/notes_view.dart
@@ -6,7 +6,7 @@ import 'package:okaychata/constants/colors.dart' show CustomColors;
 import 'package:okaychata/services/cloud/cloud_note.dart' show CloudNote;
 import 'package:okaychata/views/note/note_list_view.dart' show NoteListView;
 import 'package:okaychata/services/auth/auth_service.dart' show AuthService;
-import 'package:okaychata/services/cloud/cloud_storage.dart' show CloudStorage;
+import 'package:okaychata/services/cloud/cloud_service.dart' show CloudService;
 import 'package:okaychata/utilities/dialogs/show_logout_dialog.dart' show showLogoutDialog;
 
 class NotesView extends StatefulWidget {
@@ -17,17 +17,17 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
-  late final CloudStorage _noteService;
+  late final CloudService _noteService;
 
   String get userId => AuthService.factoryFirebase().currentUser!.id;
 
   @override
   void initState() {
-    _noteService = CloudStorage();
+    _noteService = CloudService();
     super.initState();
   }
 
-  Future<String> futureCreateUser() async {
+  Future<String> fakeDelay() async {
     return Future.delayed(const Duration(seconds: 1), () => "Delaying");
   }
 
@@ -83,40 +83,51 @@ class _NotesViewState extends State<NotesView> {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: _noteService.allNotes(ownerUserId: userId),
+      body: FutureBuilder(
+        future: fakeDelay(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-            case ConnectionState.active:
-              if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                final allNotes = snapshot.data as Iterable<CloudNote>;
+              return StreamBuilder(
+                stream: _noteService.allNotes(ownerUserId: userId),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.done:
+                    case ConnectionState.active:
+                      if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                        final allNotes = snapshot.data as Iterable<CloudNote>;
 
-                return NoteListView(
-                  notes: allNotes,
-                  onDeleteNote: (note) async {
-                    await _noteService.deleteNote(documentId: note.documentId);
-                  },
-                  onTapNote: (note) {
-                    Navigator.of(context).pushNamed(
-                      addNewNoteRoute,
-                      arguments: note,
-                    );
-                  },
-                );
-              } else {
-                return Container(
-                  color: Theme.of(context).colorScheme.background,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "There are no notes right now...",
-                    style: textTheme.labelMedium,
-                  ),
-                );
-              }
+                        return NoteListView(
+                          notes: allNotes,
+                          onDeleteNote: (note) async {
+                            await _noteService.deleteNote(documentId: note.documentId);
+                          },
+                          onTapNote: (note) {
+                            Navigator.of(context).pushNamed(
+                              addNewNoteRoute,
+                              arguments: note,
+                            );
+                          },
+                        );
+                      } else {
+                        return Container(
+                          color: Theme.of(context).colorScheme.background,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "There are no notes right now...",
+                            style: textTheme.labelMedium,
+                          ),
+                        );
+                      }
+
+                    default:
+                      return const Placeholder(color: CustomColors.transparent);
+                  }
+                },
+              );
 
             default:
-              return const Placeholder(color: CustomColors.transparent);
+              return const Center(child: CircularProgressIndicator());
           }
         },
       ),
