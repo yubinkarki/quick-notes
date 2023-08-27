88b7c612a400767e647113f4a76bd0b7d16e2807
yubinkarki <iamyubinkarki@gmail.com>
2023-08-27 16:28:10 +0545

    Add a loading state in super

--- lib/bloc/auth/auth_state.dart
+++ lib/bloc/auth/auth_state.dart
@@ -5,34 +5,36 @@ import 'package:okaychata/services/auth/auth_user.dart' show AuthUser;
 
 @immutable
 abstract class AuthState {
-  const AuthState();
+  final bool? isLoading;
+  final String? loadingText;
+
+  const AuthState({this.isLoading = false, this.loadingText = "Please wait..."});
 }
 
 class AuthStateUninitialized extends AuthState {
-  const AuthStateUninitialized();
+  const AuthStateUninitialized({bool isLoading = false}) : super(isLoading: isLoading);
 }
 
 class AuthStateRegistering extends AuthState {
   final Exception? exception;
 
-  const AuthStateRegistering(this.exception);
+  const AuthStateRegistering({this.exception, bool isLoading = false}) : super(isLoading: isLoading);
 }
 
 class AuthStateLoggedIn extends AuthState {
   final AuthUser user;
 
-  const AuthStateLoggedIn(this.user);
+  const AuthStateLoggedIn(this.user, {bool isLoading = false}) : super(isLoading: isLoading);
 }
 
 class AuthStateNeedsVerification extends AuthState {
-  const AuthStateNeedsVerification();
+  const AuthStateNeedsVerification({bool isLoading = false}) : super(isLoading: isLoading);
 }
 
 class AuthStateLoggedOut extends AuthState with EquatableMixin {
   final Exception? exception;
-  final bool isLoading;
 
-  const AuthStateLoggedOut({required this.exception, required this.isLoading});
+  const AuthStateLoggedOut({required this.exception, bool isLoading = false}) : super(isLoading: isLoading);
 
   @override
   List<Object?> get props => [exception, isLoading];
