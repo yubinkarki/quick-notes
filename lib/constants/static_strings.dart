0377485cb07d8b1a1cf1605dc97fa2889df8a2a6
yubinkarki <iamyubinkarki@gmail.com>
2023-08-29 21:28:16 +0545

    Add new string constants

--- lib/constants/static_strings.dart
+++ lib/constants/static_strings.dart
@@ -1,5 +1,10 @@
 class AppStrings {
   static const String login = "Login";
+  static const String logout = "Logout";
+  static const String nothing = "Nothing";
+  static const String addNewNote = "Add new note here";
+  static const String yourNotes = "Your Notes";
+  static const String delaying = "Delaying";
   static const String enterEmail = "Enter your email";
   static const String enterPassword = "Enter your password";
   static const String noUser = "User not found";
@@ -7,6 +12,7 @@ class AppStrings {
   static const String authError = "Authentication error";
   static const String goToRegister = "Go to Register";
   static const String goToLogin = "Go to Login";
+  static const String noNotes = "There are no notes right now...";
   static const String sendEmailVerification = "Send email verification";
   static const String emailVerificationConfirmation =
       "We've sent you an email verification. Please open it to verify your account.";
