819f84f7f08b85fb73aac923e45aa90db29eacaa
yubinkarki <iamyubinkarki@gmail.com>
2023-09-02 09:39:08 +0545

    Add forgot password state check

--- lib/views/auth/home_view.dart
+++ lib/views/auth/home_view.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext, BlocConsumer;
 
 import 'login_view.dart' show LoginView;
-import 'package:okaychata/bloc/auth/auth_state.dart';
 import 'verify_email_view.dart' show VerifyEmailView;
+import 'package:okaychata/bloc/auth/auth_state.dart';
 import 'package:okaychata/bloc/auth/auth_bloc.dart' show AuthBloc;
 import 'package:okaychata/views/note/notes_view.dart' show NotesView;
 import 'package:okaychata/views/auth/register_view.dart' show RegisterView;
 import 'package:okaychata/bloc/auth/auth_event.dart' show AuthEventInitialize;
 import 'package:okaychata/utilities/global/loading_overlay.dart' show LoadingOverlay;
+import 'package:okaychata/views/auth/forgot_password_view.dart' show ForgotPasswordView;
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -21,7 +22,6 @@ class HomeView extends StatelessWidget {
     return BlocConsumer<AuthBloc, AuthState>(
       listener: (context, state) {
         if (state.isLoading ?? false) {
-          // LoadingOverlay().show(context: context, text: state.loadingText ?? "Please wait...");
           debugPrint("${state.isLoading}");
         } else {
           LoadingOverlay().hide();
@@ -34,6 +34,8 @@ class HomeView extends StatelessWidget {
           return const VerifyEmailView();
         } else if (state is AuthStateLoggedOut) {
           return const LoginView();
+        } else if (state is AuthStateForgotPassword) {
+          return const ForgotPasswordView();
         } else if (state is AuthStateRegistering) {
           return const RegisterView();
         } else {
