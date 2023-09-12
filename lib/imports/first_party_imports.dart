f8951ab1957352b58dfc198538abf4542fc880c7
yubinkarki <iamyubinkarki@gmail.com>
2023-09-12 08:42:52 +0545

    Add all categories of main barrel import files

---
+++ lib/imports/first_party_imports.dart
@@ -0,0 +1,6 @@
+export "package:okaychata/bloc/bloc_barrel.dart";
+export "package:okaychata/themes/themes_barrel.dart";
+export "package:okaychata/services/services_barrel.dart";
+export 'package:okaychata/utilities/utilities_barrel.dart';
+export "package:okaychata/constants/constants_barrel.dart";
+export "package:okaychata/navigation/navigation_barrel.dart";
---
+++ lib/imports/third_party_imports.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:flutter_native_splash/flutter_native_splash.dart';
