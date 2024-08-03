c5e2ae6f0af51d2f4987c774ef4ea27ad728f70f
yubinkarki <iamyubinkarki@gmail.com>
2024-08-03 15:54:27 +0545

    feat: add extensions on string & double

--- lib/utilities/generics/extensions.dart
+++ lib/utilities/generics/extensions.dart
@@ -1,4 +1,4 @@
-import 'package:okaychata/imports/flutter_imports.dart' show BuildContext, ModalRoute;
+import 'package:okaychata/imports/flutter_imports.dart' show BuildContext, ModalRoute, Widget, SizedBox;
 
 // Returns a Stream of List of type T.
 extension Filter<T> on Stream<List<T>> {
@@ -25,4 +25,17 @@ extension GetArgument on BuildContext {
 
 extension Count<T extends Iterable<dynamic>> on Stream<T> {
   Stream<int> get getLength => map((T event) => event.length);
+}
+
+extension DoubleExtension on double? {
+  double validateDouble({double value = 0.0}) => this ?? 0.0;
+
+  Widget get sizedBoxWidth => SizedBox(width: this);
+  Widget get sizedBoxHeight => SizedBox(height: this);
+}
+
+extension StringExtension on String {
+  String get capitalizeFirstLetter => length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String get titleCase =>
+      replaceAll(RegExp(' +'), ' ').split(' ').map((String str) => str.capitalizeFirstLetter).join(' ');
 }
