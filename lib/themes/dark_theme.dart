dce90e3b318d95f70c6ede931a93c3d90d9d2629
yubinkarki <iamyubinkarki@gmail.com>
2024-08-05 10:22:31 +0545

    style: add global input decoration

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -1,11 +1,22 @@
-import 'package:okaychata/imports/flutter_imports.dart' show ThemeData, BuildContext, AppBarTheme, ColorScheme;
+import 'package:okaychata/imports/flutter_imports.dart'
+    show
+        ThemeData,
+        BorderSide,
+        EdgeInsets,
+        AppBarTheme,
+        ColorScheme,
+        BuildContext,
+        BorderRadius,
+        OutlineInputBorder,
+        InputDecorationTheme;
 
-import 'package:okaychata/imports/first_party_imports.dart' show CustomColors, darkTextTheme;
+import 'package:okaychata/imports/first_party_imports.dart' show CustomColors, darkTextTheme, AppSize, AppPadding;
 
 ThemeData darkTheme(BuildContext context) {
   final ThemeData base = ThemeData.dark();
 
   return base.copyWith(
+    scaffoldBackgroundColor: CustomColors.slightlyDark,
     appBarTheme: const AppBarTheme(color: CustomColors.dark),
     colorScheme: const ColorScheme.dark(
       error: CustomColors.red,
@@ -14,5 +25,24 @@ ThemeData darkTheme(BuildContext context) {
       primary: CustomColors.slightlyWhite,
     ),
     textTheme: darkTextTheme(base.textTheme),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.symmetric(horizontal: AppPadding.p16, vertical: AppPadding.p16),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.gray),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.teal),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.red),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.red),
+      ),
+    ),
   );
 }
--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -1,11 +1,22 @@
-import 'package:okaychata/imports/flutter_imports.dart' show ThemeData, BuildContext, AppBarTheme, ColorScheme;
+import 'package:okaychata/imports/flutter_imports.dart'
+    show
+        ThemeData,
+        EdgeInsets,
+        BorderSide,
+        AppBarTheme,
+        ColorScheme,
+        BuildContext,
+        BorderRadius,
+        OutlineInputBorder,
+        InputDecorationTheme;
 
-import 'package:okaychata/imports/first_party_imports.dart' show CustomColors, lightTextTheme;
+import 'package:okaychata/imports/first_party_imports.dart' show CustomColors, lightTextTheme, AppPadding, AppSize;
 
 ThemeData lightTheme(BuildContext context) {
   final ThemeData base = ThemeData.light();
 
   return base.copyWith(
+    scaffoldBackgroundColor: CustomColors.lightTeal,
     appBarTheme: const AppBarTheme(color: CustomColors.teal),
     colorScheme: const ColorScheme.light(
       error: CustomColors.red,
@@ -14,5 +25,24 @@ ThemeData lightTheme(BuildContext context) {
       surface: CustomColors.lightTeal,
     ),
     textTheme: lightTextTheme(base.textTheme),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.symmetric(horizontal: AppPadding.p16, vertical: AppPadding.p16),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.gray),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.darkTeal),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.red),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(AppSize.s10),
+        borderSide: const BorderSide(width: 1, color: CustomColors.red),
+      ),
+    ),
   );
 }
